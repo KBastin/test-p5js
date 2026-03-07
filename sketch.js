399ed86eecc6b8092827621fa525a978e2b853f4
kbastin <kbastin@gmail.com>
2026-03-07 20:37:30 +0100

    Lines on yellow square

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-    createCanvas(100,100);
+    
+    createCanvas(1000,1000);
 }
 
 function draw() {
     background("yellow");
+
+    for(let i=0; i<width; i+= 10){
+        line(i, 0, i, height);
+    }
 }
